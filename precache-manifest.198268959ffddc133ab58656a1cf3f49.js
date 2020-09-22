ba3d1b82129deb6c93e84b85bbe2185f15d86ef6
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-09-22 02:02:43 -0700

    Updates

---
+++ precache-manifest.198268959ffddc133ab58656a1cf3f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bd631adcd7bb80047cf59259babd63",
+    "url": "/redux-exercise/index.html"
+  },
+  {
+    "revision": "a0e73ff36a23bc862658",
+    "url": "/redux-exercise/static/css/main.f7a201ef.chunk.css"
+  },
+  {
+    "revision": "1c4c466aae667413c407",
+    "url": "/redux-exercise/static/js/2.d06a0153.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/redux-exercise/static/js/2.d06a0153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e73ff36a23bc862658",
+    "url": "/redux-exercise/static/js/main.e867a744.chunk.js"
+  },
+  {
+    "revision": "741f4efc5d05088f7a03",
+    "url": "/redux-exercise/static/js/runtime-main.47a7040d.js"
+  }
+]);
